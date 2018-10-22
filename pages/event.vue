12bb5d7d8722335931b4dfce95d59b58fe6a675c
Alessandro Bellanda <a.bellanda@esconsulting.it>
2018-10-22 21:45:55 +0200

    Aggiunta pagina chi siamo

--- pages/event.vue
+++ pages/event.vue
@@ -21,29 +21,18 @@
         </div>
       </a>
     </div>
-    <div class="sm-col sm-col-4 md-col-3 px2 sm-show">
-      <p class="h3">Join Us:</p>
-      <ul class="list-reset">
-        <li v-for="contact in contacts"><a class="link" target="_blank" v-bind:href="contact.href"><img class="icon" v-bind:alt="contact.alt" v-bind:src="contact.image"><span>{{contact.title}}</span></a></li>
-      </ul>
-      <p class="h3">Sponsored By:</p>
-      <ul class="list-reset">
-        <li v-for="sponsor in sponsors"><a class="link"target="_blank" v-bind:href="sponsor.href"><img class="icon" v-bind:alt="sponsor.alt" v-bind:src="sponsor.image"></a></li>
-      </ul>
-      <p class="h3" >Hosted By:</p>
-      <ul class="list-reset">
-        <li v-for="host in hosts"><a class="link" target="_blank" v-bind:href="host.href"><img class="icon" v-bind:alt="host.alt" v-bind:src="host.image"></a></li>
-      </ul>
-    </div>
+    <sidebar></sidebar>
   </div>
 </section>
 </template>
 
 <script>
 
+import Sidebar from "./sidebar";
 export default {
   name: 'event',
-  data () {
+    components: {Sidebar},
+    data () {
     return {
       events: [
         {
@@ -55,56 +44,6 @@ export default {
           link: 'https://meetup.com/CremaWeb'
         },
       ],
-      contacts: [
-        {
-            image: '/meetup.png',
-            alt: 'Meetup',
-            href: 'https://www.meetup.com/CremaWeb/',
-            title: 'CremaWeb',
-        },
-        {
-          image: '/slack.png',
-          alt: 'Slack',
-          href: 'https://join.slack.com/t/cremaweb/shared_invite/enQtMjU2NTM3ODAxMjM3LTE5ZTczMTUyMjQ2OGY4YzE0YTA4NDUxMTA0NDU5ZGQxYzA1ZWY4YTliNmY4ZTlkZjhhOTNjYzBkMWMzYjMyZTM',
-          title: 'cremaweb.slack.com',
-        },
-        {
-          image: '/facebook.png',
-          alt: 'Facebook',
-          href: 'https://www.facebook.com/cremawebcommunity',
-          title: 'cremawebcommunity',
-        },
-        {
-          image: '/twitter.png',
-          alt: 'Twitter',
-          href: 'https://twitter.com/cremaweb',
-          title: '@cremaweb',
-        },
-      ],
-      sponsors: [
-        {
-            image: '/esc.png',
-            alt: 'ESC',
-            href: 'https://esconsulting.it',
-        },
-        {
-          image: '/nealis.png',
-          alt: 'Nealis',
-          href: 'https://nealis.it',
-        },
-        {
-            image: '/idigital3.png',
-            alt: 'iDigital3',
-            href: 'http://www.idigital3.com',
-        },
-      ],
-      hosts: [
-        {
-          image: '/rei.png',
-          alt: 'Rei - Reindustria innovazione',
-          href: 'http://www.reindustria.com',
-        },
-      ]
     }
   },
   computed: {
--- pages/index.vue
+++ pages/index.vue
@@ -3,14 +3,33 @@
 </style>
 
 <template>
-
+<section class="container">
+    <div class="mxn2">
+        <div class="sm-col sm-col-8 md-col-9 px2">
+            <p class="h2">
+                Siamo un gruppo di professionisti, studenti e appassionati del mondo IT.
+            </p>
+            <p class="h3">
+                Organizziamo questo Meetup in Crema (CR) per incontrarci, discutere e condividere informazioni sui temi più svariati relativi a tecnologie Web, best practice di sviluppo, project management, startup e imprenditorialità.
+                Se sei interessato a questi argomenti e vuoi entrare in contatto con altri professionisti come te, questo gruppo fa al caso tuo.
+            </p>
+            <hr>
+            <p class="h5">
+                CremaWeb è nato come progetto senza scopo di lucro, organizziamo e portiamo avanti le nostre attività con il contributo volontario della community.
+            </p>
+        </div>
+        <sidebar></sidebar>
+    </div>
+</section>
 </template>
 
 <script>
 
+import Sidebar from "./sidebar";
 export default {
   name: 'index',
-  data () {
+    components: {Sidebar},
+    data () {
     return {
     }
   },
